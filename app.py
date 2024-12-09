45253104b408a0ff037c871c82e6090c321431fe
RomsMoss25 <134474021+RomsMoss25@users.noreply.github.com>
2024-12-09 11:45:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import dash_bootstrap_components as dbc
 
 # Cargar los datos reales desde el archivo Excel
-file_path = '/Users/romi_1/Downloads/Papers_proyectos STEM .xlsx'
+file_path = 'Papers_proyectos STEM .xlsx'
 df_real_data = pd.read_excel(file_path)
 
 # Limpiar los nombres de las columnas (opcional, elimina espacios extra)
